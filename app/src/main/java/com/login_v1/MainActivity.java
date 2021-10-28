01f762a02988d0393ec044e4c4ef3a969705abf5
asantanadurango <asantanadurango@gmail.com>
2021-10-28 00:43:31 -0500

    add view binding and spinner

---
+++ app/src/main/java/com/login_v1/MainActivity.java
@@ -0,0 +1,43 @@
+package com.login_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.login_v1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        Toast.makeText(this, "Estas en el login", Toast.LENGTH_LONG).show();
+    }
+
+    // to signup
+    public void toSignupActivity(View view){
+        Intent toSignup = new Intent(this, SecondActivity.class);
+        String text_user = binding.txtUser.getText().toString();
+        String text_password = binding.txtPassword.getText().toString();
+        toSignup.putExtra("text_user", text_user);
+        toSignup.putExtra("text_password", text_password);
+        startActivity(toSignup);
+    }
+
+    // to home
+    public void toHomeActivity(View view){
+        Intent toHome = new Intent(this, HomeActivity.class);
+        startActivity(toHome);
+    }
+
+
+
+
+}
